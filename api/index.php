1ac3dccc9c4b37afe8f92aa0f96b17bdb3394c05
Zxess <ardirafif142@gmail.com>
2026-02-16 11:28:51 +0700

    Fix storage path for Vercel with /tmp directory

--- api/index.php
+++ api/index.php
@@ -1,1 +1,9 @@
-<?php require __DIR__ . "/../public/index.php";
+﻿<?php
+$app = require __DIR__ . '/../bootstrap/vercel.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
---
+++ bootstrap/vercel.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+use Illuminate\Contracts\Console\Kernel as ConsoleKernel;
+use Illuminate\Contracts\Http\Kernel as HttpKernel;
+
+define('LARAVEL_START', microtime(true));
+
+// Load composer autoload
+require __DIR__ . '/../vendor/autoload.php';
+
+// Load environment variables
+$app = require_once __DIR__ . '/app.php';
+
+// Set storage path to /tmp for Vercel
+$app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp/storage');
+
+return $app;
